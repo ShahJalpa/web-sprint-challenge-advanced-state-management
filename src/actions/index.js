b60c82688006131f4e5659b011caf953636630a1
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-03-19 09:12:31 -0400

    reducers/actions - START and SUCESS added, also helpers added into index.js/src

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,20 @@
 import axios from 'axios';
 
+export const FETCH_SMURF_START = "FETCH_SMURF_START";
+export const FETCH_SMURF_SUCESS = "FETCH_SMURF_SUCESS";
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
+    export const fetchSmurfs = () => {
+        return dispatch => {
+            dispatch({type: FETCH_SMURF_START});
+
+            axios
+                .get('http://localhost:3333/smurfs')
+                .then((response) => {
+                    dispatch({type: FETCH_SMURF_SUCESS, payload: response.data})
+                })
+                .catch()
+        }
+    }
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
 //3. Add a standard action that allows us to set the value of the error message slice of state.
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,47 @@
-
-export const initialState = {
-}
-
-const reducer = ()=>{
-}
-
-export default reducer;
+import {FETCH_SMURF_START, FETCH_SMURF_SUCESS} from '../actions/index'
 
 //Task List:
 //1. Adds the following state values into the initialState:
 //  - an array of smurfs
 //  - a boolean indicating if the app is loading
 //  - a string indicating a possible error message
+export const initialState = {
+    smurfs: [],
+    appIsLoading: false,
+    error:""
+}
 
 //2. Add in the arguments needed to complete a standard reducer function.
-//3. Add in a reducer case to accomidate the start of a smurf fetch.
-//4. Add in a reducer case to accomidate the successful smurf api fetch.
+const reducer = (state = initialState, action)=>{
+    switch(action.type){
+        //3. Add in a reducer case to accomidate the start of a smurf fetch.
+        case(FETCH_SMURF_START):
+            return({
+                ...state,
+                appIsLoading: true,
+                error:""
+            })
+        //4. Add in a reducer case to accomidate the successful smurf api fetch.
+        case(FETCH_SMURF_SUCESS):
+            return({
+                ...state,
+                smurfs: action.payload,
+                appIsLoading: false,
+                error:''
+            })
+        default:
+            return state;
+    }
+}
+
+export default reducer;
+
+
+
+
+
+
+
 //5. Add in a reducer cases to accomidate the failed smurf api fetch.
 //6. Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
 //7. Add in a reducer case that adds in a value to the error message.
